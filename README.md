7c991add602f792211cf0fb8703ab69ef0540d0f
Ajeet <ajeetmdl23@gmail.com>
2023-06-27 16:42:58 -0400

    files added

---
+++ README.md
@@ -0,0 +1,10 @@
+### To launch mybinder jupyter nbviewer click link below:
+
+https://mybinder.org/v2/gh/ajeetmandal/my-first-binder.git/HEAD
+
+
+### To launch Rstudio click link below:
+https://mybinder.org/v2/gh/ajeetmandal/my-first-binder.git/main?urlpath=rstudio
+
+### To launch R Shiny app:
+https://mybinder.org/v2/gh/ajeetmandal/mybinder/main?urlpath=shiny/shinyR_HBCC_testData_FigShare/
---
+++ install.R
@@ -0,0 +1,16 @@
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+#BiocManager::install("iSEE")
+#BiocManager::install("SingleCellExperiment") # Dont need this as "iSEE" has this
+#BiocManager::install("scMerge")
+
+install.packages("shiny")  # Dont need this as "iSEE" has this
+
+
+# install.packages("tidyverse")
+# install.packages("rmarkdown")
+# install.packages("httr")
+# install.packages("shinydashboard")
+# install.packages('leaflet')
+
